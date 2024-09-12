7440bdca27be4bba0e87d859618d3de731263c26
zveda21 <zveda.hyan@gmail.com>
2024-09-12 22:23:33 +0400

    formatting add new methods

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,20 +1,27 @@
 package guru.qa.niffler.page;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class MainPage {
-  private final ElementsCollection tableRows = $("#spendings tbody").$$("tr");
 
-  public EditSpendingPage editSpending(String spendingDescription) {
-    tableRows.find(text(spendingDescription)).$$("td").get(5).click();
-    return new EditSpendingPage();
-  }
+    private final ElementsCollection tableRows = $("#spendings tbody").$$("tr");
+    private final SelenideElement mainPageRootContainer = $("[class^='MuiBox-root']");
 
-  public void checkThatTableContainsSpending(String spendingDescription) {
-    tableRows.find(text(spendingDescription)).should(visible);
-  }
+    public EditSpendingPage editSpending(String spendingDescription) {
+        tableRows.find(text(spendingDescription)).$$("td").get(5).click();
+        return new EditSpendingPage();
+    }
+
+    public void checkThatTableContainsSpending(String spendingDescription) {
+        tableRows.find(text(spendingDescription)).should(visible);
+    }
+
+    public void checkThatPageContainsRootContainer() {
+        mainPageRootContainer.shouldBe(visible);
+    }
 }
