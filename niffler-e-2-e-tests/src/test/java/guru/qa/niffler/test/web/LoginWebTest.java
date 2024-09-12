d6a1ff3358532bdd577fa79c54aeeb70130148b1
zveda21 <zveda.hyan@gmail.com>
2024-09-12 22:24:34 +0400

    add test cases for Login and Registry Pages

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginWebTest.java
@@ -0,0 +1,31 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.BrowserExtension;
+import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.page.MainPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BrowserExtension.class)
+public class LoginWebTest {
+
+    private static final Config CFG = Config.getInstance();
+
+    @Test
+    public void mainPageShouldBeDisplayedAfterSuccessLogin() {
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("duck", "12345");
+        new MainPage()
+                .checkThatPageContainsRootContainer();
+    }
+
+    @Test
+    public void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
+        String badCredentialsMessage = "Bad credentials";
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .login("duck", "123456")
+                .checkIfErrorMessageDisplayed(badCredentialsMessage);
+    }
+}
---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/RegistrationWebTest.java
@@ -0,0 +1,65 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import com.github.javafaker.Faker;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.BrowserExtension;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BrowserExtension.class)
+public class RegistrationWebTest {
+
+    private static final Config CFG = Config.getInstance();
+    private final Faker faker = new Faker();
+
+    @Test
+    public void shouldRegisterNewUser() {
+        String username = faker.name().username();
+        String password = faker.internet().password(3, 12);
+        String registrationSuccessMessage = "Congratulations! You've registered!";
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .clickOnCreateNewAccountButton()
+                .setUsername(username)
+                .setPassword(password)
+                .setPasswordSubmit(password)
+                .submitRegistration()
+                .checkThatSuccessRegistryMessageDisplayed(registrationSuccessMessage);
+
+    }
+
+    @Test
+    public void shouldNotRegisterUserWithExistingUsername() {
+        String username = "existinguser";
+        String password = faker.internet().password(3, 12);
+        String existingErrorMessage = "Username " + "`" + username + "` already exists";
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .clickOnCreateNewAccountButton()
+                .setUsername(username)
+                .setPassword(password)
+                .setPasswordSubmit(password)
+                .submitRegistration()
+                .checkThatErrorMessageDisplayed(existingErrorMessage);
+
+    }
+
+    @Test
+    public void shouldShowErrorIfPasswordAndConfirmPasswordAreNotEqual() {
+        String username = faker.name().username();
+        String password = faker.internet().password(3, 12);
+        String confirmPassword = faker.internet().password(4,12);
+        String differentPasswordErrorMessage = "Passwords should be equal";
+
+        Selenide.open(CFG.frontUrl(), LoginPage.class)
+                .clickOnCreateNewAccountButton()
+                .setUsername(username)
+                .setPassword(password)
+                .setPasswordSubmit(confirmPassword)
+                .submitRegistration()
+                .checkThatErrorMessageDisplayed(differentPasswordErrorMessage);
+
+    }
+}
