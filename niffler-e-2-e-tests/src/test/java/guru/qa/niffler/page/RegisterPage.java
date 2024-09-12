921283dc9908788aff4d4b35ae41b258253a9052
zveda21 <zveda.hyan@gmail.com>
2024-09-12 22:24:00 +0400

    add RegisterPage PageObject

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/RegisterPage.java
@@ -0,0 +1,44 @@
+package guru.qa.niffler.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegisterPage {
+
+    private final SelenideElement usernameInput = $("#username");
+    private final SelenideElement passwordInput = $("#password");
+    private final SelenideElement submitPasswordInput = $("#passwordSubmit");
+    private final SelenideElement signUpButton = $("button.form__submit");
+    private final SelenideElement errorMessage = $(".form__error");
+    private final SelenideElement successRegistryMessage = $("[class^='form__paragraph']");
+
+    public RegisterPage setUsername(String username) {
+        usernameInput.setValue(username);
+        return this;
+    }
+
+    public RegisterPage setPassword(String password) {
+        passwordInput.setValue(password);
+        return this;
+    }
+
+    public RegisterPage setPasswordSubmit(String password) {
+        submitPasswordInput.setValue(password);
+        return this;
+    }
+
+    public RegisterPage submitRegistration() {
+        signUpButton.click();
+        return this;
+    }
+
+    public void checkThatErrorMessageDisplayed(String error) {
+        errorMessage.shouldHave(text(error));
+    }
+
+    public void checkThatSuccessRegistryMessageDisplayed(String successMessage) {
+        successRegistryMessage.shouldHave(text(successMessage));
+    }
+}
