eca20d97c60ca387fb578455b95fdcbb6f835086
zveda21 <zveda.hyan@gmail.com>
2024-09-18 20:29:05 +0400

    modify annotations based on task

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Category.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/Category.java
@@ -12,7 +12,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @ExtendWith(CategoryExtension.class)
 public @interface Category {
-  String username();
+
+  String username() default "";
 
   boolean archived() default false;
 
