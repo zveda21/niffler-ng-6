ada57a13734db2fba933d4ed12db7ae222aef9fe
zveda21 <zveda.hyan@gmail.com>
2024-09-25 01:29:49 +0400

    modify SpendDbClient based on task

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -8,19 +8,47 @@ import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
 import guru.qa.niffler.model.SpendJson;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public class SpendDbClient {
 
-  private final SpendDao spendDao = new SpendDaoJdbc();
-  private final CategoryDao categoryDao = new CategoryDaoJdbc();
+    private final SpendDao spendDao = new SpendDaoJdbc();
+    private final CategoryDao categoryDao = new CategoryDaoJdbc();
 
-  public SpendJson createSpend(SpendJson spend) {
-    SpendEntity spendEntity = SpendEntity.fromJson(spend);
-    if (spendEntity.getCategory().getId() == null) {
-      CategoryEntity categoryEntity = categoryDao.create(spendEntity.getCategory());
-      spendEntity.setCategory(categoryEntity);
+    public SpendJson createSpend(SpendJson spend) {
+        SpendEntity spendEntity = SpendEntity.fromJson(spend);
+        if (spendEntity.getCategory().getId() == null) {
+            CategoryEntity categoryEntity = categoryDao.create(spendEntity.getCategory());
+            spendEntity.setCategory(categoryEntity);
+        }
+        return SpendJson.fromEntity(
+                spendDao.create(spendEntity)
+        );
     }
-    return SpendJson.fromEntity(
-        spendDao.create(spendEntity)
-    );
-  }
+
+    public SpendJson findSpendById(UUID spendId) {
+        Optional<SpendEntity> optionalSpend = spendDao.findSpendById(spendId);
+        return optionalSpend.map(SpendJson::fromEntity).orElse(null);
+    }
+
+    public List<SpendJson> findAllSpendByUsername(String username) {
+        List<SpendEntity> spendEntities = spendDao.findAllByUsername(username);
+        List<SpendJson> spendJsonList = new ArrayList<>();
+
+        for (SpendEntity spendEntity : spendEntities) {
+            spendJsonList.add(SpendJson.fromEntity(spendEntity));
+        }
+        return spendJsonList;
+    }
+
+    public void deleteSpending(SpendJson spend) {
+        if (spend == null) {
+            throw new IllegalArgumentException("SpendJson cannot be null");
+        }
+        SpendEntity spendEntity = SpendEntity.fromJson(spend);
+        spendDao.deleteSpend(spendEntity);
+    }
 }
