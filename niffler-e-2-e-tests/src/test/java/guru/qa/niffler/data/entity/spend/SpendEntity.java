43cb0e7c6e54c5450e97b113da071cdf1ac10e7e
zveda21 <zveda.hyan@gmail.com>
2024-09-25 01:28:35 +0400

    modify SpendEntity based on task

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/entity/spend/SpendEntity.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/entity/spend/SpendEntity.java
@@ -12,27 +12,27 @@ import java.util.UUID;
 @Getter
 @Setter
 public class SpendEntity implements Serializable {
-  private UUID id;
-  private String username;
-  private CurrencyValues currency;
-  private Date spendDate;
-  private Double amount;
-  private String description;
-  private CategoryEntity category;
+    private UUID id;
+    private String username;
+    private CurrencyValues currency;
+    private Date spendDate;
+    private Double amount;
+    private String description;
+    private CategoryEntity category;
 
-  public static SpendEntity fromJson(SpendJson json) {
-    SpendEntity se = new SpendEntity();
-    se.setId(json.id());
-    se.setUsername(json.username());
-    se.setCurrency(json.currency());
-    se.setSpendDate(new java.sql.Date(json.spendDate().getTime()));
-    se.setAmount(json.amount());
-    se.setDescription(json.description());
-    se.setCategory(
-        CategoryEntity.fromJson(
-            json.category()
-        )
-    );
-    return se;
-  }
+    public static SpendEntity fromJson(SpendJson json) {
+        SpendEntity se = new SpendEntity();
+        se.setId(json.id());
+        se.setUsername(json.username());
+        se.setCurrency(json.currency());
+        se.setSpendDate(new java.sql.Date(json.spendDate().getTime()));
+        se.setAmount(json.amount());
+        se.setDescription(json.description());
+        se.setCategory(
+                CategoryEntity.fromJson(
+                        json.category()
+                )
+        );
+        return se;
+    }
 }
