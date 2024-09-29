04c025449aa2f889281560be0f1ca6b9beeed7c3
zveda21 <zveda.hyan@gmail.com>
2024-09-29 22:48:47 +0400

    modify based on task

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/entity/auth/AuthUserEntity.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/entity/auth/AuthUserEntity.java
@@ -4,11 +4,12 @@ import guru.qa.niffler.model.AuthUserJson;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class AuthUserEntity {
+public class AuthUserEntity implements Serializable {
 
     private UUID id;
     private String username;
