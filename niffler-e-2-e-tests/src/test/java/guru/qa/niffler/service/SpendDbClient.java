a4ad63bf55ab6ab73c1145d5f28f6b85b46857f0
zveda21 <zveda.hyan@gmail.com>
2024-09-29 15:41:31 +0400

    modify based on task

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -1,57 +1,74 @@
 package guru.qa.niffler.service;
 
-import guru.qa.niffler.data.dao.CategoryDao;
-import guru.qa.niffler.data.dao.SpendDao;
+import guru.qa.niffler.config.Config;
 import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
 import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
 import guru.qa.niffler.model.SpendJson;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static guru.qa.niffler.data.Databases.transaction;
+
 public class SpendDbClient {
 
-    private final SpendDao spendDao = new SpendDaoJdbc();
-    private final CategoryDao categoryDao = new CategoryDaoJdbc();
+    private static final Config CFG = Config.getInstance();
+    private static final int TRANSACTION_ISOLATION_LEVEL = Connection.TRANSACTION_READ_COMMITTED;
 
     public SpendJson createSpend(SpendJson spend) {
-        SpendEntity spendEntity = SpendEntity.fromJson(spend);
-        Optional<CategoryEntity> existingCategory = categoryDao.findCategoryById(spendEntity.getCategory().getId());
-        if (existingCategory.isPresent()) {
-            spendEntity.setCategory(existingCategory.get());
-        } else {
-            CategoryEntity categoryEntity = categoryDao.create(spendEntity.getCategory());
-            spendEntity.setCategory(categoryEntity);
-        }
-        return SpendJson.fromEntity(
-                spendDao.create(spendEntity)
+        return transaction(connection -> {
+                    SpendEntity spendEntity = SpendEntity.fromJson(spend);
+                    if (spendEntity.getCategory().getId() == null) {
+                        CategoryEntity categoryEntity = new CategoryDaoJdbc(connection)
+                                .create(spendEntity.getCategory());
+                        spendEntity.setCategory(categoryEntity);
+                    }
+                    return SpendJson.fromEntity(
+                            new SpendDaoJdbc(connection).create(spendEntity)
+                    );
+                },
+                CFG.spendJdbcUrl(),
+                TRANSACTION_ISOLATION_LEVEL
         );
     }
 
     public SpendJson findSpendById(UUID spendId) {
-        Optional<SpendEntity> optionalSpend = spendDao.findSpendById(spendId);
-        return optionalSpend.map(SpendJson::fromEntity).orElse(null);
+        return transaction(connection -> {
+                    Optional<SpendEntity> optionalSpend = new SpendDaoJdbc(connection).findSpendById(spendId);
+                    return optionalSpend.map(SpendJson::fromEntity).orElse(null);
+                },
+                CFG.spendJdbcUrl(),
+                TRANSACTION_ISOLATION_LEVEL);
     }
 
     public List<SpendJson> findAllSpendByUsername(String username) {
-        List<SpendEntity> spendEntities = spendDao.findAllByUsername(username);
-        List<SpendJson> spendJsonList = new ArrayList<>();
-
-        for (SpendEntity spendEntity : spendEntities) {
-            spendJsonList.add(SpendJson.fromEntity(spendEntity));
-        }
-        return spendJsonList;
+        return transaction(connection -> {
+                    List<SpendEntity> spendEntities = new SpendDaoJdbc(connection).findAllByUsername(username);
+                    List<SpendJson> spendJsonList = new ArrayList<>();
+                    for (SpendEntity spendEntity : spendEntities) {
+                        spendJsonList.add(SpendJson.fromEntity(spendEntity));
+                    }
+                    return spendJsonList;
+                },
+                CFG.spendJdbcUrl(),
+                TRANSACTION_ISOLATION_LEVEL);
     }
 
     public void deleteSpending(SpendJson spend) {
         if (spend == null) {
             throw new IllegalArgumentException("SpendJson cannot be null");
         }
-        SpendEntity spendEntity = SpendEntity.fromJson(spend);
-        spendDao.deleteSpend(spendEntity);
+        transaction(connection -> {
+                    SpendEntity spendEntity = SpendEntity.fromJson(spend);
+                    new SpendDaoJdbc(connection).deleteSpend(spendEntity);
+                    return null;
+                },
+                CFG.spendJdbcUrl(),
+                TRANSACTION_ISOLATION_LEVEL);
     }
 }
