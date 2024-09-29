54efae0026e0b16b4aebcc1e9d7da372b272b3b1
zveda21 <zveda.hyan@gmail.com>
2024-09-29 15:40:47 +0400

    modify based on task

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/Databases.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/Databases.java
@@ -1,33 +1,177 @@
 package guru.qa.niffler.data;
 
-import org.postgresql.ds.PGSimpleDataSource;
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.jdbc.AtomikosDataSourceBean;
+import jakarta.transaction.SystemException;
+import jakarta.transaction.UserTransaction;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Databases {
-  private Databases() {
-  }
+    private static final Map<String, DataSource> dataSources = new ConcurrentHashMap<>();
+    private static final Map<Long, Map<String, Connection>> threadConnections = new ConcurrentHashMap<>();
 
-  private static final Map<String, DataSource> dataSources = new ConcurrentHashMap<>();
+    private Databases() {
+    }
 
-  private static DataSource dataSource(String jdbcUrl) {
-    return dataSources.computeIfAbsent(
-        jdbcUrl,
-        key -> {
-          PGSimpleDataSource ds = new PGSimpleDataSource();
-          ds.setUser("postgres");
-          ds.setPassword("secret");
-          ds.setUrl(key);
-          return ds;
+    // Modified to include isolation level
+    public static <T> T transaction(Function<Connection, T> function, String jdbcUrl, int isolationLvl) {
+        Connection connection = null;
+        try {
+            connection = connection(jdbcUrl);
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(isolationLvl);
+            T result = function.apply(connection);
+            connection.commit();
+            connection.setAutoCommit(true);
+            return result;
+        } catch (Exception e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            throw new RuntimeException(e);
         }
-    );
-  }
+    }
 
-  public static Connection connection(String jdbcUrl) throws SQLException {
-    return dataSource(jdbcUrl).getConnection();
-  }
+    public static <T> T xaTransaction(int isolationLevel, XaFunction<T>... actions) {
+        UserTransaction ut = new UserTransactionImp();
+        try {
+            ut.begin();
+            T result = null;
+            for (XaFunction<T> action : actions) {
+                Connection connection = connection(action.jdbcUrl);
+                connection.setTransactionIsolation(isolationLevel);
+                action.function.apply(connection);
+            }
+            ut.commit();
+            return result;
+        } catch (Exception e) {
+            try {
+                ut.rollback();
+            } catch (SystemException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void transaction(Consumer<Connection> consumer, String jdbcUrl, int isolationLvl) {
+        Connection connection = null;
+        try {
+            connection = connection(jdbcUrl);
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(isolationLvl);
+            consumer.accept(connection);
+            connection.commit();
+            connection.setAutoCommit(true);
+        } catch (Exception e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    connection.setAutoCommit(true);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void xaTransaction(int isolationLevel, XaConsumer... actions) {
+        UserTransaction ut = new UserTransactionImp();
+        try {
+            ut.begin();
+            for (XaConsumer action : actions) {
+                Connection connection = connection(action.jdbcUrl);
+                connection.setTransactionIsolation(isolationLevel);
+                action.function.accept(connection);
+            }
+            ut.commit();
+        } catch (Exception e) {
+            try {
+                ut.rollback();
+            } catch (SystemException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static DataSource dataSource(String jdbcUrl) {
+        return dataSources.computeIfAbsent(
+                jdbcUrl,
+                key -> {
+                    AtomikosDataSourceBean dsBean = new AtomikosDataSourceBean();
+                    final String uniqId = StringUtils.substringAfter(jdbcUrl, "5432/");
+                    dsBean.setUniqueResourceName(uniqId);
+                    dsBean.setXaDataSourceClassName("org.postgresql.xa.PGXADataSource");
+                    Properties props = new Properties();
+                    props.put("URL", jdbcUrl);
+                    props.put("user", "postgres");
+                    props.put("password", "secret");
+                    dsBean.setXaProperties(props);
+                    dsBean.setMaxPoolSize(20);
+                    return dsBean;
+                }
+        );
+    }
+
+    private static Connection connection(String jdbcUrl) throws SQLException {
+        return threadConnections.computeIfAbsent(
+                Thread.currentThread().threadId(),
+                key -> {
+                    try {
+                        return new HashMap<>(Map.of(
+                                jdbcUrl,
+                                dataSource(jdbcUrl).getConnection()
+                        ));
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ).computeIfAbsent(
+                jdbcUrl,
+                key -> {
+                    try {
+                        return dataSource(jdbcUrl).getConnection();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
+
+    public static void closeAllConnections() {
+        for (Map<String, Connection> connectionMap : threadConnections.values()) {
+            for (Connection connection : connectionMap.values()) {
+                try {
+                    if (connection != null && !connection.isClosed()) {
+                        connection.close();
+                    }
+                } catch (SQLException e) {
+                    // NOP
+                }
+            }
+        }
+    }
+
+    public record XaFunction<T>(Function<Connection, T> function, String jdbcUrl) {
+    }
+
+    public record XaConsumer(Consumer<Connection> function, String jdbcUrl) {
+    }
 }
