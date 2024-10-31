b8cec1e5bec518f9db0b063f3055643203e3bfb2
zveda21 <zveda.hyan@gmail.com>
2024-10-31 23:40:18 +0400

    modify test method

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/EmptyUsersTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/EmptyUsersTest.java
@@ -2,21 +2,22 @@ package guru.qa.niffler.test.web;
 
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.rest.UserJson;
-import guru.qa.niffler.service.UsersClient;
 import guru.qa.niffler.service.impl.UsersApiClient;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Order(1)
 public class EmptyUsersTest {
 
     @Test
     @User
-    void checkIfUsersListIsEmpty(UserJson user){
-        UsersClient usersClient = new UsersApiClient();
-        List<UserJson> response = usersClient.getAllUsers(user.username(),null);
-        assertTrue(response.isEmpty(),"The users list is empty");
+    void checkIfUsersListIsEmptyTest(UserJson user){
+        UsersApiClient usersApiClient = new UsersApiClient();
+        List<UserJson> response = usersApiClient.getAllUsers(user.username(),null);
+        assertTrue(response.isEmpty(),"The user list must be empty.");
     }
 }
