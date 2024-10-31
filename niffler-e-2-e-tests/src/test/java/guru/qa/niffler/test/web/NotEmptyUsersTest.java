0c771760de592994c07c865303aa03c78980ad95
zveda21 <zveda.hyan@gmail.com>
2024-10-31 23:39:59 +0400

    add NotEmptyUsersTest test

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/NotEmptyUsersTest.java
@@ -0,0 +1,23 @@
+package guru.qa.niffler.test.web;
+
+import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.model.rest.UserJson;
+import guru.qa.niffler.service.impl.UsersApiClient;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@Order(Integer.MAX_VALUE)
+public class NotEmptyUsersTest {
+
+    @User
+    @Test
+    void checkIfUsersListIsNotEmptyTest(UserJson user) {
+        UsersApiClient usersApiClient = new UsersApiClient();
+        List<UserJson> response = usersApiClient.getAllUsers(user.username(), null);
+        assertFalse(response.isEmpty(), "The user list must not be empty.");
+    }
+}
