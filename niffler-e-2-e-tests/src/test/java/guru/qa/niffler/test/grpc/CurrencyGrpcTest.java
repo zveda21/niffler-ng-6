51a9cc0b658037997f60c181a8f0ab67a0d9023a
zveda21 <zveda.hyan@gmail.com>
2025-01-28 22:36:37 +0400

    add homework 25

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/grpc/CurrencyGrpcTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/grpc/CurrencyGrpcTest.java
@@ -1,12 +1,13 @@
 package guru.qa.niffler.test.grpc;
 
 import com.google.protobuf.Empty;
-import guru.qa.niffler.grpc.Currency;
-import guru.qa.niffler.grpc.CurrencyResponse;
+import guru.qa.niffler.grpc.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CurrencyGrpcTest extends BaseGrpcTest {
 
@@ -14,6 +15,54 @@ public class CurrencyGrpcTest extends BaseGrpcTest {
   void allCurrenciesShouldReturned() {
     final CurrencyResponse response = blockingStub.getAllCurrencies(Empty.getDefaultInstance());
     final List<Currency> allCurrenciesList = response.getAllCurrenciesList();
-    Assertions.assertEquals(4, allCurrenciesList.size());
+    assertEquals(4, allCurrenciesList.size());
+  }
+
+  @Test
+  void calculateRateShouldReturnCorrectAmountForUSDToEUR() {
+    double amount = 34.00;
+    CurrencyValues spendCurrency = CurrencyValues.USD;
+    CurrencyValues changeToCurrency = CurrencyValues.EUR;
+
+    CalculateRequest request = CalculateRequest.newBuilder()
+            .setAmount(amount)
+            .setSpendCurrency(spendCurrency)
+            .setDesiredCurrency(changeToCurrency)
+            .build();
+
+    CalculateResponse response = blockingStub.calculateRate(request);
+    assertEquals(31.48, response.getCalculatedAmount(), 0.01);
+  }
+
+  @Test
+  void calculateRateShouldReturnCorrectAmountForUSDToRUB() {
+    double amount = 100;
+    CurrencyValues spendCurrency = CurrencyValues.USD;
+    CurrencyValues changeToCurrency = CurrencyValues.RUB;
+
+    CalculateRequest request = CalculateRequest.newBuilder()
+            .setAmount(amount)
+            .setSpendCurrency(spendCurrency)
+            .setDesiredCurrency(changeToCurrency)
+            .build();
+
+    CalculateResponse response = blockingStub.calculateRate(request);
+    assertEquals(6666.67, response.getCalculatedAmount(), 0.01);
+  }
+
+  @Test
+  void calculateRateShouldReturnSameAmountForRUBToRUB() {
+    double amount = 250;
+    CurrencyValues spendCurrency = CurrencyValues.RUB;
+    CurrencyValues changeToCurrency = CurrencyValues.RUB;
+
+    CalculateRequest request = CalculateRequest.newBuilder()
+            .setAmount(amount)
+            .setSpendCurrency(spendCurrency)
+            .setDesiredCurrency(changeToCurrency)
+            .build();
+
+    CalculateResponse response = blockingStub.calculateRate(request);
+    assertEquals(250.0, response.getCalculatedAmount(), 0.01);
   }
 }
